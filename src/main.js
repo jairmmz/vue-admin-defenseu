275e3022f82b0e9ab5391208c91da0118d67cd88
jairmmz <jaimumiran@gmail.com>
2023-10-23 17:20:53 -0500

    Refactorización del proyecto 1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 import App from './App.vue'
 
 import './css/style.css'
 
 const app = createApp(App)
+const pinia = createPinia()
+
 app.use(router)
+app.use(pinia)
+pinia.use(piniaPluginPersistedstate)
 app.mount('#app')
